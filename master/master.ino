be067301545027a99e42024636f32aa34b0da938
Cole Morgan <cole.morgan@utexas.edu>
2020-02-01 12:30:18 -0600

    IMU, Zigbee RX working

--- master/master.ino
+++ master/master.ino
@@ -1,3 +1,32 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+#define BAUD_RATE 9600
+#define ZIGBEE_SERIAL Serial3
+#define ZIGBEE_SERIAL_TX 7 // Can be 0, 21
+#define ZIGBEE_SERIAL_RX 8 // Can be 1, 5
+#define BLE_SERIAL Serial2
+#define BLE_SERIAL_TX 9 // Can be 9, 26
+#define BLE_SERIAL_RX 10 // Can be 10, 31
+
+// Current quaternion data
+//     0
+//     |
+//     1 
+//     |
+//     2
+//     |
+//     3
+//     |
+// 4 - 5 - 6
+
+float quats[7][4];
+bool newQuatData = false;
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 float quatDiff(float q[4], float r[4]) {
   float qDiff[4] = {0};
   float rConj[4] = {r[0], -r[1], -r[2], -r[3]};
@@ -6,7 +35,6 @@ float quatDiff(float q[4], float r[4]) {
   qDiff[1] = (rConj[0]*q[1]+rConj[1]*q[0]-rConj[2]*q[3]+rConj[3]*q[2]);
   qDiff[2] = (rConj[0]*q[2]+rConj[1]*q[3]+rConj[2]*q[0]-rConj[3]*q[1]);
   qDiff[3] = (rConj[0]*q[3]-rConj[1]*q[2]+rConj[2]*q[1]+rConj[3]*q[0]);
-  
   
   float norm = (qDiff[0]*qDiff[0]+qDiff[1]*qDiff[1]+qDiff[2]*qDiff[2]+qDiff[3]*qDiff[3]);
   norm = sqrt(norm);
@@ -20,12 +48,130 @@ float quatDiff(float q[4], float r[4]) {
   return angle;
 }
 
-void setup() {
-  // put your setup code here, to run once:
+int minDataLength = 22;
 
+void printQuats(void) {
+  for(int i = 0; i < 7; i++) {
+    Serial.print("Device #"); Serial.println(i);
+    for(int j = 0; j < 4; j++) { 
+      Serial.print("Q");Serial.print(j); Serial.print(": "); Serial.println(quats[i][j]);
+    }
+  }
 }
 
+void parseZigbeeData(String s)
+{
+  if(s.length() == 0 || s.length() < minDataLength) 
+  {
+    s = "";
+    return;
+  }
+
+  String currentString = s;
+  String subarray = "";
+  int moduleID;
+  //Serial.print("This is the substring: ");
+  
+  // Parse Module ID
+  subarray = currentString.substring(0, s.indexOf("{"));
+  moduleID = subarray.toInt();
+  
+  // Parse q0
+  Serial.print("q0:");
+  subarray = currentString.substring(currentString.indexOf("{")+1, currentString.indexOf(","));
+  Serial.println(subarray);
+  quats[moduleID][0] = subarray.toFloat();
+  currentString = currentString.substring(currentString.indexOf(",")+1);
+//  Serial.print(currentString);
+
+  // Parse q1
+  Serial.print("q1:");
+  subarray = currentString.substring(0, currentString.indexOf(","));
+  Serial.println(subarray);
+  quats[moduleID][1] = subarray.toFloat();
+  currentString = currentString.substring(currentString.indexOf(",")+1);
+//  Serial.print(currentString);
+  
+  // Parse q2
+  Serial.print("q2:");
+  subarray = currentString.substring(0, currentString.indexOf(","));
+  Serial.println(subarray);
+  quats[moduleID][2] = subarray.toFloat();
+  currentString = currentString.substring(currentString.indexOf(",")+1);
+//  Serial.print(currentString);
+
+  // Parse q3
+  Serial.print("q3:");
+  subarray = currentString.substring(0, currentString.indexOf("}"));
+  Serial.println(subarray);
+  quats[moduleID][3] = subarray.toFloat();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  ZIGBEE_SERIAL.begin(BAUD_RATE); 
+  BLE_SERIAL.begin(BAUD_RATE); 
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.write("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+}
+
+String dataLine;
 void loop() {
   // put your main code here, to run repeatedly:
+  // Get local quaternion
+  imu::Quaternion quat = bno.getQuat();
+//  Serial.print("qW: ");
+//  Serial.print(quat.w(), 4);
+//  Serial.print(" qX: ");
+//  Serial.print(quat.x(), 4);
+//  Serial.print(" qY: ");
+//  Serial.print(quat.y(), 4);
+//  Serial.print(" qZ: ");
+//  Serial.print(quat.z(), 4);
+//  Serial.print("\n");
+  // Read from Zigbee serial if available
+  char c;
+  
+  if(ZIGBEE_SERIAL.available())
+  {
+    c = ZIGBEE_SERIAL.read();
+    if(c == '\n')
+    {
+      //Serial.write(dataLine.c_str());
+      //Serial.write("\n");
+      Serial.println(dataLine);
+      parseZigbeeData(dataLine);
+      printQuats();
+      dataLine = "";
+      newQuatData = true;
+    }
+    else
+    {
+      dataLine += c; //append text to end of command
+    }
+  }
+  // Transmit angles through BLE
+  if(newQuatData) {
+    String uartTx = ""; // "AT+BLEUARTTX={";
+    for(int i = 0; i < 5; i++) {
+      uartTx += quatDiff(quats[i], quats[i+1]);
+      uartTx += ",";
+    }
+    uartTx += quatDiff(quats[5], quats[6]);
+    // uartTx += "}";
+    BLE_SERIAL.write(uartTx.c_str());
 
+    newQuatData = false;
+  }
 }
